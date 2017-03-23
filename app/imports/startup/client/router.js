fd30a4e2878072e140f70093ae3966532c8151dd
samrasay <samrasay@hawaii.edu>
2017-03-22 23:18:36 -1000

    Finished mockup

--- app/imports/startup/client/router.js
+++ app/imports/startup/client/router.js
@@ -22,6 +22,42 @@ FlowRouter.route('/add', {
   },
 });
 
+FlowRouter.route('/add-schedule', {
+  name: 'Add_Schedule_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'Add_Schedule_Page' });
+  },
+});
+
+FlowRouter.route('/gym-hours', {
+  name: 'Gym_Hours_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'Gym_Hours_Page' });
+  },
+});
+
+FlowRouter.route('/machines-open', {
+  name: 'Machines_Open_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'Machines_Open_Page' });
+  },
+});
+
+FlowRouter.route('/log-page', {
+  name: 'Log_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'Log_Page' });
+  },
+});
+
+FlowRouter.route('/workout-schedule-page', {
+  name: 'Workout_Schedule_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'Workout_Schedule_Page' });
+  },
+});
+
+
 FlowRouter.route('/stuff/:_id', {
   name: 'Edit_Stuff_Page',
   action() {
--- app/imports/ui/pages/index.js
+++ app/imports/ui/pages/index.js
@@ -1,3 +1,18 @@
+import './add-schedule-page.html';
+import './add-schedule-page.js';
+
+import './gym_hours_page.html';
+import './gym_hours_page.js';
+
+import './machines_open_page.html';
+import './machines_open_page.js';
+
+import './workout-schedule-page.html';
+import './workout-schedule-page.js';
+
+import './log_page.html';
+import './log_page.js';
+
 import './add-stuff-page.html';
 import './add-stuff-page.js';
 
